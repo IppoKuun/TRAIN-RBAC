63ac3177a63269cbd0eaf3dc99f1a5ecc689fb26
Hippolyte <hippolyte.devweb@gmail.com>
2026-02-28 12:43:43 +0100

    feat add vercel functionnality

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,8 @@
-import app from "../app.js";
+// backend/api/index.js
+import app, { ensureDbConnected } from "../app.js";
 
-export default app;
+export default async (req, res) => {
+    // On s'assure que la DB est connectée avant de traiter la requête
+    await ensureDbConnected();
+    return app(req, res);
+};
